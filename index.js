b2505e2592cb42bef4a4329c19149182ff610347
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-10-17 19:12:38 -0400

    inquirer prompt

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquire');
+const fs = require('fs');
+const generateLogo = require('./lib/generateLogo')
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        {}
+        const logo = 
+    })
