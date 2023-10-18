d1dbabbdb7611e35d37945b361319d549f0bd1b7
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-10-17 20:12:06 -0400

    questions

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const inquirer = require('inquire');
 const fs = require('fs');
 const generateLogo = require('./lib/GenerateLogo');
+
+const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Square','Circle','Triangle']
+    },
+    {
+        type: 'list',
+        name: 'color',
+        message: 'Choose a background color:',
+        choices: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+    },
+    {
+        type: 'maxlength-input',
+        message: 'Please enter three characters.',
+        name: 'text',
+        maxLength: 3,
+    },
+]
 inquirer
     .prompt(questions)
     .then((answers) => {
         ({shape, color, text} = answers);
-        const logo = generateLogo
-        fs.writeFile('./output/logo.svg')
+        const logo = generateLogo;
+        fs.writeFile('./output/logo.svg');
     })
     
